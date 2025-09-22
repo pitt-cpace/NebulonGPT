435682bbe3b20415c89aa2e45ce847981914b64b
Mohammadreza Moradi <mrmoradi68@gmail.com>
2025-09-22 11:36:02 -0400

    Removing unuseful Vosk server file

--- start-vosk-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Vosk Speech Recognition Server...
-echo.
-echo Make sure you have Python and the required dependencies installed:
-echo   pip install vosk websockets
-echo.
-echo Starting server on localhost:2700 (internal, accessible via localhost:3000/vosk)...
-cd Vosk-Server\websocket
-python asr_server_with_models.py models
